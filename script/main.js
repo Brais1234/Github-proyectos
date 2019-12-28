fe99f9918cb01fc9bc8cd9596e5dede39c1ea21c
Miguel4421 <59318347+Miguel4421@users.noreply.github.com>
2019-12-28 20:28:36 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,1 +1,89 @@
 
+// Image switcher code
+
+
+
+let myImage = document.querySelector('img');
+
+
+
+myImage.onclick = function() {
+
+  let mySrc = myImage.getAttribute('src');
+
+  if(mySrc === 'images/amsterdam.png') {
+
+    myImage.setAttribute ('src','images/berlín.png');
+
+  } else {
+
+    myImage.setAttribute ('src','images/amsterdam.png');
+
+  }
+
+}
+
+
+
+// Personalized welcome message code
+
+
+
+let myButton = document.querySelector('button');
+
+let myHeading = document.querySelector('h1');
+
+
+
+function setUserName() {
+
+  let myName = prompt('Please enter your name.');
+
+  if(!myName || myName === null) {
+
+    setUserName();
+
+  } else {
+
+    localStorage.setItem('name', myName);
+
+    myHeading.innerHTML = 'Viaje por Europa' + myName;
+
+  }
+
+}
+
+
+
+if(!localStorage.getItem('name')) {
+
+  setUserName();
+
+} else {
+
+  let storedName = localStorage.getItem('name');
+
+  myHeading.innerHTML = 'Viaje por Europa' + storedName;
+
+}
+
+
+
+myButton.onclick = function() {
+
+  setUserName();
+
+}
+
+
+   var catEl = document.getElementById("cat");
+ 
+  var startTime = new Date().getTime();
+  var walkTheCat = function() {
+      var currTime = new Date().getTime();
+      var secondsElapsed = ((currTime - startTime)/1000);
+   var newLeft= secondsElapsed*77 + "px";
+    catEl.style.left= newLeft;
+    window.requestAnimationFrame(walkTheCat);
+  };
+  walkTheCat();
